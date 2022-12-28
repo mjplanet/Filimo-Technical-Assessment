57794899fe9aedf013e84701188d643d4fc98c83
Mobin Jahantark <jahantark@me.com>
2022-12-29 00:00:34 +0330

    Add - Search movie collectionView created.

--- FilimoTechnicalAssessment/SearchMovies/SearchMoviesView.swift
+++ FilimoTechnicalAssessment/SearchMovies/SearchMoviesView.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 protocol SearchMoviesViewInterface: AnyObject {
+    func updateSnapshot(from movies: [String])
 }
 
 class SearchMoviesView: UIViewController {
     
+    // MARK: - Properties
     var presenter: SearchMoviesPresenterInterface
+    private var dataSource: UICollectionViewDiffableDataSource<Section, String>?
     
+    private enum Section {
+        case main
+    }
+    
+    private lazy var movieSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    // MARK: - Inits
     init(presenter: SearchMoviesPresenterInterface) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
@@ -22,27 +42,78 @@ class SearchMoviesView: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Properties
-    
-    
-    
+        
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDidLoad()
+        configureHierarchy()
+        configureDataSource()
+        updateSnapshot(from: ["Test"])
     }
     
-    
     // MARK: - Setups
-    
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.delegate = self
+        collectionView.register(MovieCell.self, forCellWithReuseIdentifier: MovieCell.identifier)
+        view.addSubview(collectionView)
+    }
 
+    private func createLayout() -> UICollectionViewLayout {
+        let sectionProvider = { [weak self] (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            return self.createCollectionSection()
+        }
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.interSectionSpacing = 16
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: configuration)
+    }
     
+    private func createCollectionSection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(92))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(92))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 8
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+        
+        return section
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, movieItem -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell
+            cell?.movieName = "Test"
+            cell?.releaseDate = "300"
+//            cell?.name = movieItem.
+//            cell?.year = movieItem.year
+            return cell
+        })
+    }
     // MARK: - Actions
     
 }
 
-
-
 extension SearchMoviesView: SearchMoviesViewInterface {
+    func updateSnapshot(from movies: [String]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(movies)
+        dataSource?.apply(snapshot)
+    }
+}
+
+extension SearchMoviesView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
 }
